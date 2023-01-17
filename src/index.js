01e2f5d33954dba763858f3ab0a3a5d4d6b93cd3
Angelica1970 <angelbaldwin013@gmail.com>
2023-01-16 21:26:51 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, Link} from 'react-router-dom';
+import Login from './Login';
 
 const App =() => {
 const[registerUsername,setRegisterUsername] = useState('');
@@ -9,7 +10,7 @@ const[loginUsername,setLoginUsername] = useState('');
 const[loginPassword,setLoginPassword] = useState('');
 const[user,setUser] = useState({});
 
-useEffect(()=> {
+const exchangeTokenForUser = ()=> {
   const token = window.localStorage.getItem('token');
   if(token){
     fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/me', {
@@ -25,50 +26,12 @@ useEffect(()=> {
       })
       .catch(err => console.log(err));
   }
+};
+useEffect(()=> {
+  exchangeTokenForUser();
 },[]);
   
-  
-  const login =(ev) =>{
-    ev.preventDefault();
-    
-    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/login', {
-  method: "POST",
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    user: {
-      username: loginUsername,
-      password: loginPassword
-      }
-  })
-})
-.then(response => response.json())
-.then(result => {
-  if(!result.success){
-    throw result.error;
-  }
-    const token = result.data.token
-    window.localStorage.setItem('token',token);
-    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/me', {
-   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-  },
-}).then(response => response.json())
-  .then(result => {
-    const user = result.data;
-    setUser(user);
-  })
-  .catch(console.error);
-    
-   console.log(result);
-
-    })
-  .catch(err => console.log(err));
-  }
-
-  const register = (ev)=>{
+   const register = (ev)=>{
     ev.preventDefault();
     fetch('https://strangers-things.herokuapp.com/api//2209-FTB-ET-WEB-AM/users/register', {
   method: "POST",
@@ -118,17 +81,7 @@ useEffect(()=> {
         />
         <button>Register</button>
       </form>
-      <form onSubmit = { login }>
-        <input placeholder='unsername'
-        value ={loginUsername} 
-        onChange = {ev =>setLoginUsername(ev.target.value)}
-        />
-        <input placeholder='password' 
-        value ={loginPassword} 
-        onChange = {ev =>setLoginPassword(ev.target.value)}
-        />
-        <button>Login</button>
-      </form>
+      <Login exchangeTokenForUser = {exchangeTokenForUser}/>
       </div>) : null
       } 
     </div>
